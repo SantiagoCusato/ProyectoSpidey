704fb4a278dbbb30571f997518a04ebf250345c4
Ramiro <rmeza@alumnos.exa.unicen.edu.ar>
2023-10-03 11:38:44 -0300

    responsive

--- TP2/EntregaFinal/js/header.js
+++ TP2/EntregaFinal/js/header.js
@@ -1,18 +1,22 @@
 "use strict";
 
 document.addEventListener('DOMContentLoaded',function menuOn(){
+    let main = document.querySelector("main");
 
     let menuCategorias = document.querySelector(".categorias");
 
     let botonMenuCategorias = document.querySelector(".btn-menu");//Mobile
     botonMenuCategorias.addEventListener("click", function(){
         menuCategorias.classList.toggle("mostrar-categorias");
+        main.classList.toggle("segundo-plano");
     });
 
     let menuUsuario = document.querySelector(".menu-usuario");
 
+
     let botonMenuUsuario = document.querySelector(".btn-usuario");
     botonMenuUsuario.addEventListener("click", function(){
         menuUsuario.classList.toggle("mostrar-menu-usuario");
+        main.classList.toggle("segundo-plano");
     });
 });

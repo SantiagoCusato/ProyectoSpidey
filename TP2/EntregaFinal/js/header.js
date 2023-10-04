d8469f124cc88fdbbbc3185d35b3629f245a6842
Ramiro <rmeza@alumnos.exa.unicen.edu.ar>
2023-10-03 22:19:28 -0300

    animacion bnt-menu

--- TP2/EntregaFinal/js/header.js
+++ TP2/EntregaFinal/js/header.js
@@ -7,16 +7,26 @@ document.addEventListener('DOMContentLoaded',function menuOn(){
 
     let botonMenuCategorias = document.querySelector(".btn-menu");//Mobile
     botonMenuCategorias.addEventListener("click", function(){
+        if(menuUsuario.classList.contains("mostrar-menu-usuario")){
+            menuUsuario.classList.toggle("mostrar-menu-usuario");
+        }else{
+            main.classList.toggle("segundo-plano");
+        }
         menuCategorias.classList.toggle("mostrar-categorias");
-        main.classList.toggle("segundo-plano");
+        botonMenuCategorias.classList.toggle("active");
     });
 
     let menuUsuario = document.querySelector(".menu-usuario");
 
-
     let botonMenuUsuario = document.querySelector(".btn-usuario");
     botonMenuUsuario.addEventListener("click", function(){
+        if(menuCategorias.classList.contains("mostrar-categorias")){
+            menuCategorias.classList.toggle("mostrar-categorias");
+        }else{
+            main.classList.toggle("segundo-plano");
+        }
         menuUsuario.classList.toggle("mostrar-menu-usuario");
-        main.classList.toggle("segundo-plano");
+        
+        
     });
 });

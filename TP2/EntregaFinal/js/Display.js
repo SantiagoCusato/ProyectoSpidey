621fd2e5b2e3df2687a7ab98b7dd78a634ea085a
Ramiro <rmeza@alumnos.exa.unicen.edu.ar>
2023-10-08 20:36:57 -0300

    imagenes, cambiar ancho mobile

---
+++ TP2/EntregaFinal/js/Display.js
@@ -0,0 +1,6 @@
+class Display{
+    constructor(botonIzq, botonDer, slider){
+
+    }
+
+}
--- TP2/EntregaFinal/js/carousel.js
+++ TP2/EntregaFinal/js/carousel.js
@@ -1,111 +1,107 @@
-var container = document.getElementById('container')
-var slider = document.getElementById('slider');
-var slides = document.getElementsByClassName('slide');
-var slides2 = document.querySelectorAll('.slide');
-var cantidadSlides = slides.length;
-var buttons = document.getElementsByClassName('btn');
+let displays = Array.from(document.getElementsByClassName('display'));
 
-var currentPosition = 0;
-var currentMargin = 0;
-var slidesPerPage = 0;
-var slidesCount = cantidadSlides - slidesPerPage;
-var containerWidth = container.offsetWidth;
-var prevKeyActive = false;
-var nextKeyActive = true;
+let map = new Map();
 
-let movPorcentual = 0;
-let steps = 0;
+const movPorcentual = 62;
 
-var rect = slides[1].getBoundingClientRect();
-var rect2 = slides[2].getBoundingClientRect();
-var cardLToCardL = rect2.x - rect.x;
+function setParams() {
+    displays.forEach(e => {
+        e.children[1].children[0].style.marginLeft =  0 + '%';
+    });
 
-window.addEventListener("resize", checkWidth);
-
-function checkWidth() {
-    containerWidth = container.offsetWidth;
-    setParams(screen.width);
+    displays.forEach(e => {
+        let datoPosicion = {
+            "margenActual" : 0,
+            "posicionActual" : 0,
+            "maxPos" : 3
+        }
+        map.set(e, datoPosicion);
+    });
 }
 
-function setParams(w) {
-    if(typeof w === 'undefined'){
-        w=screen.width;
-    }
-    if (w < 768) {
-        slidesPerPage = 1;
-        //100.8
-        movPorcentual = 100.8;
-        steps = 14;
-    } else {
-        slidesPerPage=5;
-        movPorcentual = 64;
-        steps = 3;
-    }
-    slidesCount = cantidadSlides - slidesPerPage;
-    if (currentPosition > slidesCount) {
-        currentPosition -= slidesPerPage;
-    };
-    currentMargin = - currentPosition * (100 / slidesPerPage);
-    slider.style.marginLeft = currentMargin + '%';
-    if (currentPosition > 0) {
-        buttons[0].classList.remove('inactive');
-    }
-    if (currentPosition < slidesCount) {
-        buttons[1].classList.remove('inactive');
-    }
-    if (currentPosition >= slidesCount) {
-        buttons[1].classList.add('inactive');
-    }
-}
+function slideRight(b) {
+    let buttonL = null;
+    let buttonR = null;
+    let sliderTemp = null;
+    let datPosTemp = null;
 
-setParams();
+    displays.forEach(e => {
+        if(e.contains(b)){
+            datPosTemp = map.get(e);
 
+            buttonL = b;
+            buttonR = e.children[2];
+            sliderTemp = e.children[1].children[0];
+        }
+    });
 
-function slideRight() {
-    if (currentPosition != 0) {
-        Array.from(slides).forEach((element) => {
+    if (datPosTemp.posicionActual != 0) {
+        Array.from(sliderTemp.children).forEach((element) => {
             element.classList.toggle('mov-derecha');
         });
-        slider.style.marginLeft = currentMargin + (movPorcentual) + '%';
-        currentMargin += (movPorcentual);
-        currentPosition--;
+
+
+        sliderTemp.style.marginLeft = datPosTemp.margenActual + (movPorcentual) + '%';
+        datPosTemp.margenActual += (movPorcentual);
+        datPosTemp.posicionActual--;
+
+
+
         setTimeout(() => {
-            Array.from(slides).forEach((element) => {
+            Array.from(sliderTemp.children).forEach((element) => {
                 element.classList.toggle('mov-derecha');
             });
-          }, 1000);
-    };
-    if (currentPosition === 0) {
-        buttons[0].classList.add('flecha-out');
+        }, 1000);
     }
-    if (currentPosition < slidesCount) {
-        buttons[1].classList.remove('flecha-out');
+
+    if (datPosTemp.posicionActual === 0) {
+        buttonL.classList.add('flecha-out');
+    }
+    if (datPosTemp.posicionActual < datPosTemp.maxPos) {
+        buttonR.classList.remove('flecha-out');
     }
 };
 
-function slideLeft() {
-    if (currentPosition < steps) {
-        Array.from(slides).forEach((element) => {
+function slideLeft(b) {
+    let buttonL = null;
+    let buttonR = null;
+    let sliderTemp = null;
+    let datPosTemp = null;
+
+    displays.forEach(e => {
+        if(e.contains(b)){
+            datPosTemp = map.get(e);
+
+            buttonL = e.children[0];
+            buttonR = b;
+            sliderTemp = e.children[1].children[0];
+        }
+    });
+
+    if (datPosTemp.posicionActual < datPosTemp.maxPos) {
+        Array.from(sliderTemp.children).forEach((element) => {
             element.classList.toggle('mov-izquierda');
         });
-        slider.style.marginLeft = currentMargin - (movPorcentual) + '%';
-        currentMargin -= (movPorcentual);
-        currentPosition++;
+
+
+        sliderTemp.style.marginLeft = datPosTemp.margenActual - (movPorcentual) + '%';
+        datPosTemp.margenActual -= (movPorcentual);
+        datPosTemp.posicionActual++;
+
 
         setTimeout(() => {
-            Array.from(slides).forEach((element) => {
+            Array.from(sliderTemp.children).forEach((element) => {
                 element.classList.toggle('mov-izquierda');
             });
-          }, 1000);
+        }, 1000);
+    }
 
-        
-        
-    };
-    if (currentPosition == steps) {
-        buttons[1].classList.add('flecha-out');
+    if (datPosTemp.posicionActual == datPosTemp.maxPos) {
+        buttonR.classList.add('flecha-out');
     }
-    if (currentPosition > 0) {
-        buttons[0].classList.remove('flecha-out');
+    if (datPosTemp.posicionActual > 0) {
+        buttonL.classList.remove('flecha-out');
     }
-    
 };
+
+setParams();

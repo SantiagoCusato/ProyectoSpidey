6ace005b1b166df9eb1759e36866df4b5656b02f
Ramiro <rmeza@alumnos.exa.unicen.edu.ar>
2023-10-09 17:50:54 -0300

    cambiar nombre index

--- TP2/EntregaFinal/js/cargando.js
+++ TP2/EntregaFinal/js/cargando.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded',function menuOn(){
-    const HOME_URL = "https://ramitafcex.github.io/InterfacesDeUsuario.github.io/TP2/EntregaFinal/index.html"
+    const HOME_URL = "https://ramitafcex.github.io/InterfacesDeUsuario.github.io/TP2/EntregaFinal/home.html"
     setTimeout(() => {
         location.href = HOME_URL;
     }, 4800);

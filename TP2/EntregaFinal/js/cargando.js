37f4823e64d9d81d9fe40503bad6a50819328e88
Ramiro <rmeza@alumnos.exa.unicen.edu.ar>
2023-10-09 22:18:41 -0300

    fix header, main pagina juego

--- TP2/EntregaFinal/js/cargando.js
+++ TP2/EntregaFinal/js/cargando.js
@@ -2,5 +2,5 @@ document.addEventListener('DOMContentLoaded',function menuOn(){
     const HOME_URL = "https://ramitafcex.github.io/InterfacesDeUsuario.github.io/TP2/EntregaFinal/home.html"
     setTimeout(() => {
         location.href = HOME_URL;
-    }, 4800);
+    }, 4500);
 });

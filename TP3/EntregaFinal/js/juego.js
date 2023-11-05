d5b3d9bd6bd3921e69b0d5b7dc73b4ca714b8583
Ramiro <rmeza@alumnos.exa.unicen.edu.ar>
2023-11-05 15:22:14 -0300

    enlaces fixed

--- TP3/EntregaFinal/js/juego.js
+++ TP3/EntregaFinal/js/juego.js
@@ -2,12 +2,9 @@
 document.addEventListener('DOMContentLoaded',function menuOn(){
     const PAG_JUEGO = "https://ramitafcex.github.io/InterfacesDeUsuario.github.io/TP2/EntregaFinal/paginaJuego.html";
     let cardJuego = Array.from(document.getElementsByClassName('a-pag-juego'));
-
     cardJuego.forEach(e => {
         e.addEventListener("click", function(){
             location.href = PAG_JUEGO;
         });
     });
-
-
 });
